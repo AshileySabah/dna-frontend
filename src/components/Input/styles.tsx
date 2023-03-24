f410232900d49c6659733b95071ca12e1278dd22
AshileySabah <ashileysabrina2002@gmail.com>
2023-03-24 20:58:02 -0300

    refactor: change image and replace component

--- src/components/Input/styles.tsx
+++
@@ -1,8 +0,0 @@
-import { styled } from "@mui/material/styles";
-import { TextField as TextFieldMui } from "@mui/material";
-
-export const TextField = styled(TextFieldMui)`
-  .MuiInputBase-root.MuiOutlinedInput-root {
-    padding-right: 0;
-  }
-`;
