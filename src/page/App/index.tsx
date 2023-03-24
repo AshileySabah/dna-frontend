894515e4e40b91a630ea5b4ca0ad947d45fa7fdb
AshileySabah <ashileysabrina2002@gmail.com>
2023-03-24 12:44:53 -0300

    feat: matrix interface

--- src/page/App/index.tsx
+++ src/page/App/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/no-array-index-key */
-import React, { useCallback, useEffect } from "react";
+import React, { useEffect } from "react";
 import { useForm } from "react-hook-form";
 
 import { yupResolver } from "@hookform/resolvers/yup";
@@ -10,14 +10,23 @@ import { validation } from "./validation";
 import { Input } from "../../components/Input";
 import { useSnackbar } from "../../hooks/Snackbar";
 
+const createMatrix = (dimension: number) => {
+  const newMatrix: string[][] = [];
+  for (let i = 0; i < Number(dimension); i++) {
+    const row = new Array(Number(dimension))?.fill("");
+    newMatrix?.push(row);
+  }
+  return newMatrix;
+};
+
 interface IDNATest {
   dimension: number;
-  matrix: string[];
+  matrix: string[][];
 }
 
 const defaultValues: IDNATest = {
   dimension: 3,
-  matrix: [],
+  matrix: createMatrix(3),
 };
 
 export const App = () => {
@@ -45,28 +54,12 @@ export const App = () => {
 
   const matrixFieldWidth = 100 / dimension;
   const showAsRow = matrixFieldWidth < 10;
-  const showMatrix = matrix?.length && dimension >= defaultValues?.dimension;
+  const showMatrix =
+    matrix?.length > 0 && dimension >= defaultValues?.dimension;
 
   useEffect(() => {
-    const newMatrix = new Array(dimension ** 2)?.fill("");
-    setValue("matrix", newMatrix);
+    setValue("matrix", createMatrix(dimension));
   }, [dimension]);
-
-  // implementar see as row or matrix
-
-  const buildMultidimentionalMatrix = useCallback(() => {
-    const matrixIndexesList: number[] = [];
-    matrix?.forEach((item, index) => {
-      const isRowBeggining = (index + dimension) % dimension === 0;
-      const isRowEnd = (index + 1) % dimension === 0;
-
-      if (isRowBeggining || isRowEnd) {
-        matrixIndexesList?.push(index);
-      }
-    });
-
-    console.log(matrixIndexesList);
-  }, [matrix]);
 
   return (
     <Box sx={{ backgroundColor: "lightblue", height: "calc(100vh - 72px)" }}>
@@ -101,7 +94,7 @@ export const App = () => {
               </Button>
             </Grid>
             <Grid item xs={12} sm={6} md={6}>
-              <Button onClick={() => buildMultidimentionalMatrix()}>
+              <Button onClick={() => console.log(matrix)}>
                 Submit DNA test
               </Button>
             </Grid>
@@ -111,27 +104,21 @@ export const App = () => {
 
       {showMatrix &&
         (showAsRow ? (
-          <Grid container spacing={1} mt={2}>
-            {matrix?.map((_value, index) => {
-              const isRowBeggining = (index + dimension) % dimension === 0;
-              const isRowEnd = (index + 1) % dimension === 0;
-
-              return (
-                <>
-                  {isRowBeggining && (
-                    <Box width="100%" mt={3}>
-                      <Typography variant="h6">Row X</Typography>
+          <Grid container spacing={1} mt={2} justifyContent="center">
+            <Grid item xs={12}>
+              {matrix?.map((row, rowIndex) => (
+                <Box key={rowIndex} display="flex">
+                  {row?.map((item, itemIndex) => (
+                    <Box key={itemIndex}>
+                      <Input
+                        name={`matrix.${rowIndex}.${itemIndex}`}
+                        control={control}
+                      />
                     </Box>
-                  )}
-                  <Input
-                    name={`matrix.${index}`}
-                    control={control}
-                    label="Matrix"
-                  />
-                  {isRowEnd && <Box style={{ width: "100%" }} />}
-                </>
-              );
-            })}
+                  ))}
+                </Box>
+              ))}
+            </Grid>
           </Grid>
         ) : (
           <Grid container spacing={1} mt={2} justifyContent="center">
@@ -139,24 +126,21 @@ export const App = () => {
               item
               xs={12}
               sm={12}
-              md={matrixFieldWidth <= 12.5 ? 8 : 6}
+              md={12}
               sx={{ backgroundColor: "lightgrey" }}
             >
-              <Box
-                display="flex"
-                flexWrap="wrap"
-                sx={{ backgroundColor: "tomato" }}
-              >
-                {matrix?.map((_value, index) => (
-                  <Box key={index} sx={{ width: `${matrixFieldWidth}%` }}>
-                    <Input
-                      name={`matrix.${index}`}
-                      control={control}
-                      label="Matrix"
-                    />
-                  </Box>
-                ))}
-              </Box>
+              {matrix?.map((row, rowIndex) => (
+                <Box key={rowIndex} display="flex">
+                  {row?.map((item, itemIndex) => (
+                    <Box key={itemIndex}>
+                      <Input
+                        name={`matrix.${rowIndex}.${itemIndex}`}
+                        control={control}
+                      />
+                    </Box>
+                  ))}
+                </Box>
+              ))}
             </Grid>
           </Grid>
         ))}
