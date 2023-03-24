5e1afa2961e848beb9984dab2610f5d1002784fa
AshileySabah <ashileysabrina2002@gmail.com>
2023-03-24 18:14:16 -0300

    feat: component - button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { ButtonProps } from "@mui/material";
+import { Container } from "./styles";
+
+interface IButtonProps extends ButtonProps {
+  loading?: boolean;
+}
+
+export const Button: React.FC<IButtonProps> = ({ loading, ...buttonProps }) => {
+  const { color, variant, endIcon, startIcon, children } = buttonProps ?? {};
+
+  return (
+    <Container
+      {...buttonProps}
+      disableElevation
+      color={color ?? "primary"}
+      variant={variant ?? "contained"}
+      disabled={loading}
+      endIcon={endIcon}
+      startIcon={startIcon}
+    >
+      {loading ? "Carregando..." : children}
+    </Container>
+  );
+};
---
+++ src/components/Button/styles.tsx
@@ -0,0 +1,6 @@
+import { styled } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+
+export const Container = styled(Button)`
+  min-height: 40px;
+`;
